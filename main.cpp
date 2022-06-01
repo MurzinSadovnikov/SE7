10015b90d4f4f52d8791c07ddf09955cbcdf3007
MurzinSadovnikov <danila.murzin.98@mail.ru>
2022-06-01 21:01:07 +0500

    fourth commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-    int x, y;
+    int x, y, s;
 
     cout << "Enter X: "; cin >> x;
     cout << "Enter Y: "; cin >> y;
+
+    s=x+y;
+    cout << "Sum="; cout << s;
 
     return 0;
 }
