5b7339adfb247c41a3100a47ee326cb49e17d23d
MurzinSadovnikov <danila.murzin.98@mail.ru>
2022-06-01 22:04:44 +0500

    Seventh commit Dobavlenie raznosti

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-    int x, y, s;
+    int x, y, s, r;
 
     cout << "Enter X: "; cin >> x;
     cout << "Enter Y: "; cin >> y;
 
     s=x+y;
     cout << "Sum="; cout << s;
+
+    r=x-y;
+    cout << "Raznost'="; cout << r;
 
     return 0;
 }
